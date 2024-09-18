3a9ec36b3191d6532a961ebafe242c7918ce0dee
farsuller <farsuller2020@gmail.com>
2024-09-18 21:39:01 +0800

    update build gradle, added debug folder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.solodev.codingchallenge"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.solodev.codingchallenge"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
